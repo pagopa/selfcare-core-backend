7d26a4457054e81c0b8a3c1445fd927324789992
mtedahfo <marve.dullain.tedah.fosso.st@everis.nttdata.com>
2021-10-21 18:28:55 +0200

    Test Product Controller

--- web/src/test/java/it/pagopa/selfcare/product/web/ProductControllerTest.java
+++ web/src/test/java/it/pagopa/selfcare/product/web/ProductControllerTest.java
@@ -6,8 +6,8 @@ import it.pagopa.selfcare.product.core.ProductService;
 import it.pagopa.selfcare.product.dao.model.Product;
 import it.pagopa.selfcare.product.web.model.CreateProductDto;
 import it.pagopa.selfcare.product.web.model.ProductResource;
+import it.pagopa.selfcare.product.web.model.UpdateProductDto;
 import it.pagopa.selfcare.product.web.utils.TestUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +23,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(value = {ProductController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @ContextConfiguration(classes = {
@@ -32,19 +35,13 @@ class ProductControllerTest {
 
     private static final String BASE_URL = "/products";
     private static final CreateProductDto CREATE_PRODUCT_DTO;
+    private static final UpdateProductDto UPDATE_PRODUCT_DTO;
     private static final Product PRODUCT;
 
     static {
         CREATE_PRODUCT_DTO = TestUtils.mockInstance(new CreateProductDto());
-//        CREATE_PRODUCT_DTO = new CreateProductDto();
-//        CREATE_PRODUCT_DTO.setDescription("Description");
-//        CREATE_PRODUCT_DTO.setLogo("Logo");
-//        CREATE_PRODUCT_DTO.setTitle("Title");
-//        CREATE_PRODUCT_DTO.setUrlBO("UrlBO");
-//        CREATE_PRODUCT_DTO.setUrlPublic("UrlPublic");
-
+        UPDATE_PRODUCT_DTO = TestUtils.mockInstance(new UpdateProductDto());
         PRODUCT = TestUtils.mockInstance(new Product());
-//        PRODUCT.setId(UUID.randomUUID().toString());
     }
 
     @MockBean
@@ -64,20 +61,18 @@ class ProductControllerTest {
                 .thenReturn(Collections.singletonList(PRODUCT));
         // when
         MvcResult result = mvc.perform(MockMvcRequestBuilders
-                .get(BASE_URL + "/")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
+                        .get(BASE_URL + "/")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andReturn();
         // then
         List<ProductResource> products = objectMapper.readValue(
                 result.getResponse().getContentAsString(),
-                new TypeReference<List<ProductResource>>() {
+                new TypeReference<>() {
                 });
-        Assertions.assertNotNull(products);
-        Assertions.assertFalse(products.isEmpty());
-        // TODO: check that all expected ProductResult fields are populated
-        Assertions.assertEquals(PRODUCT.getId(), products.get(0).getId());
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
     }
 
     @Test
@@ -87,24 +82,23 @@ class ProductControllerTest {
                 .thenReturn(Collections.emptyList());
         // when
         MvcResult result = mvc.perform(MockMvcRequestBuilders
-                .get(BASE_URL + "/")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
+                        .get(BASE_URL + "/")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andReturn();
         // then
         List<ProductResource> products = objectMapper.readValue(
                 result.getResponse().getContentAsString(),
-                new TypeReference<List<ProductResource>>() {
+                new TypeReference<>() {
                 });
-        Assertions.assertNotNull(products);
-        Assertions.assertTrue(products.isEmpty());
-        // TODO: check that all expected ProductResult fields are populated
+        assertNotNull(products);
+        assertTrue(products.isEmpty());
     }
 
 
     @Test
-    void getProduct() {
+    void getProductNotNull() throws Exception {
         // given
         Mockito.when(productServiceMock.getProduct(Mockito.anyString()))
                 .thenAnswer(invocationOnMock -> {
@@ -113,21 +107,114 @@ class ProductControllerTest {
                     p.setId(id);
                     return p;
                 });
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/id")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
         // then
-        // TODO: check that all expected ProductResult fields are populated
-//        ProductResource resource = objectMapper.readValue(result.getResponse().getContentAsString(), ProductResource.class);
+        ProductResource product = objectMapper.readValue(result.getResponse().getContentAsString(), ProductResource.class);
+        assertNotNull(product);
     }
 
     @Test
-    void createProduct() {
+    void getProductNull() throws Exception {
+        // given
+        Mockito.when(productServiceMock.getProduct(Mockito.anyString()))
+                .thenReturn(null);
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/id")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        assertEquals("", result.getResponse().getContentAsString());
     }
 
     @Test
-    void updateProduct() {
+    void createProduct() throws Exception {
+        // given
+        Mockito.when(productServiceMock.createProduct(Mockito.any(Product.class)))
+                .thenAnswer(invocationOnMock -> {
+                    Product product = invocationOnMock.getArgument(0, Product.class);
+                    product.setId(UUID.randomUUID().toString());
+                    return product;
+                });
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .post(BASE_URL + "/")
+                        .content(objectMapper.writeValueAsString(CREATE_PRODUCT_DTO))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        ProductResource product = objectMapper.readValue(result.getResponse().getContentAsString(), ProductResource.class);
+        assertNotNull(product);
+        assertNotNull(product.getId());
+        TestUtils.reflectionEqualsByName(CREATE_PRODUCT_DTO, product, "id");
     }
 
     @Test
-    void deleteProduct() {
+    void updateProductNotNul() throws Exception {
+        // given
+        Product prod = new Product();
+        Mockito.when(productServiceMock.updateProduct(Mockito.anyString(), Mockito.any(Product.class)))
+                .thenAnswer(invocationOnMock -> {
+                    String id = invocationOnMock.getArgument(0, String.class);
+                    Product product = invocationOnMock.getArgument(1, Product.class);
+                    product.setId(id);
+                    return product;
+                });
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .put(BASE_URL + "/id")
+                        .content(objectMapper.writeValueAsString(UPDATE_PRODUCT_DTO))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        ProductResource product = objectMapper.readValue(result.getResponse().getContentAsString(), ProductResource.class);
+
+        assertNotNull(product);
+        TestUtils.reflectionEqualsByName(UPDATE_PRODUCT_DTO, product, "id");
+    }
+
+    @Test
+    void updateProductNull() throws Exception {
+        // given
+        Mockito.when(productServiceMock.updateProduct(Mockito.anyString(), Mockito.any(Product.class)))
+                .thenReturn(null);
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .put(BASE_URL + "/id")
+                        .content(objectMapper.writeValueAsString(UPDATE_PRODUCT_DTO))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        assertEquals("", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void deleteProduct() throws Exception {
+        // given
+        productServiceMock.deleteProduct(Mockito.anyString());
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .delete(BASE_URL + "/id")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        assertEquals("", result.getResponse().getContentAsString());
     }
 
 }
