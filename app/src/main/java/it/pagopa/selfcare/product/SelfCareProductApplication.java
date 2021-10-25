e11a725b03d83ce263e99b28973d4c78b6fe5c04
Rodolfo Viti <rodoviti@everis.com>
2021-10-25 10:48:20 +0200

    add OpenAPI file

--- app/src/main/java/it/pagopa/selfcare/product/SelfCareProductApplication.java
+++ app/src/main/java/it/pagopa/selfcare/product/SelfCareProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SelfCareDashboardApplication {
+public class SelfCareProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SelfCareDashboardApplication.class, args);
+        SpringApplication.run(SelfCareProductApplication.class, args);
     }
 
 }
