9f33380df33b33f88f0c9858087c8f1cbb9bbd74
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-06-29 11:26:15 +0200

    save product depict image controller fix

--- web/src/main/java/it/pagopa/selfcare/product/web/controller/ProductController.java
+++ web/src/main/java/it/pagopa/selfcare/product/web/controller/ProductController.java
@@ -79,17 +79,18 @@ public class ProductController {
         return null;
     }
 
-    @PutMapping(value = "/{id}/depict-image")
+    @PutMapping(value = "/{id}/depict-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "", notes = "${swagger.product.operation.saveProductDepictImage}")
-    public void saveProductDepictImage(@ApiParam("${swagger.product.model.id}")
-                                       @PathVariable("id") String id,
-                                       @ApiParam("${swagger.product.model.depictImage}")
-                                       @RequestParam("depictImage") MultipartFile depictImage) throws IOException {
+    public Object saveProductDepictImage(@ApiParam("${swagger.product.model.id}")
+                                         @PathVariable("id") String id,
+                                         @ApiParam("${swagger.product.model.depictImage}")
+                                         @RequestParam("depictImage") MultipartFile depictImage) throws IOException {
         log.trace("saveProductDepictImage start");
         log.debug("saveProductDepictImage id = {}, logo = {}", id, depictImage);
         productService.saveProductDepictImage(id, depictImage.getInputStream(), depictImage.getContentType(), depictImage.getOriginalFilename());
         log.trace("saveProductDepictImage end");
+        return null;
     }
 
 
