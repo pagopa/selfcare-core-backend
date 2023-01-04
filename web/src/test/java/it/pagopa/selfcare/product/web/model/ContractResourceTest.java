6c5ef67763d0ebb2eb52dc0c569eee1cfe9da7e7
bgalgamu <kevin.si1396@gmail.com>
2023-01-04 17:26:28 +0100

    add for contract resource

---
+++ web/src/test/java/it/pagopa/selfcare/product/web/model/ContractResourceTest.java
@@ -0,0 +1,60 @@
+package it.pagopa.selfcare.product.web.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static it.pagopa.selfcare.commons.utils.TestUtils.mockInstance;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContractResourceTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @Test
+    void validateNullFields() {
+        // given
+        HashMap<String, Class<? extends Annotation>> toCheckMap = new HashMap<>();
+        toCheckMap.put("contractTemplatePath", NotBlank.class);
+        toCheckMap.put("contractTemplateVersion", NotBlank.class);
+        toCheckMap.put("contractTemplateUpdatedAt", NotNull.class);
+        ContractResource contractResource = new ContractResource();
+        // when
+        Set<ConstraintViolation<Object>> violations = validator.validate(contractResource);
+        // then
+        List<ConstraintViolation<Object>> filteredViolations = violations.stream()
+                .filter(violation -> {
+                    Class<? extends Annotation> annotationToCheck = toCheckMap.get(violation.getPropertyPath().toString());
+                    return !violation.getConstraintDescriptor().getAnnotation().annotationType().equals(annotationToCheck);
+                })
+                .collect(Collectors.toList());
+        assertTrue(filteredViolations.isEmpty(), filteredViolations.toString());
+    }
+
+    @Test
+    void validateNotNullFields() {
+        // given
+        ContractResource resource = mockInstance(new ContractResource());
+        // when
+        Set<ConstraintViolation<Object>> violations = validator.validate(resource);
+        // then
+        assertTrue(violations.isEmpty(), violations.toString());
+    }
+}
